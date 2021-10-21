0f752013c54fa330cd3b5f2776a491d3185ac341
Siddhartha Singh <masterwww03@gmail.com>
2021-10-21 11:52:27 +0530

    Find area of rectangle

---
+++ Exercise_6.c
@@ -0,0 +1,35 @@
+/*
+Challenge
+
+Write a program that gets two "Double Number":
+1.=> Rectangle Height.
+2.=> Rectangle Width.
+
+The program should calculate and print the area of the given rectangle.
+
+*/
+
+#include<stdio.h>
+int main()
+{
+        // Always assign variables in small letters 
+
+    double height, width, area;
+        // Variable type double means it contains integer and float both values.
+
+    printf("Enter the height of Rectangle: ");
+    scanf("%lf",&height);
+
+        // %lf insted of %d or %f because %lf conatains both the values.
+
+    printf("Enter the Width of Rectangle: ");
+    scanf("%lf",&width);
+
+    area = height * width;
+
+        // %2.lf num 2 use because i want to print only two digits after dot like 58.22.
+
+    printf("Area of Rectangle is: %2.lf\n",area);
+
+    return 0;
+}
