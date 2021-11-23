a242dfcb1ab7994313e21a98b17ed2a9f53b29e6
Siddhartha Singh <masterwww03@gmail.com>
2021-11-23 21:32:57 +0530

    C Program

---
+++ Condition_And_Control_Flow/Relatinal_operators.c
@@ -0,0 +1,23 @@
+/*
+        Relational Operators
+
+1=> Constructing Conditions
+    - "if grade is greater than 80..."
+    - Construct condition in our programing language
+
+2=> Some Relational Operators:
+    >  - Greater than
+    >= - Greater than(or equal to)
+    <  - Less than
+    <= - Less than(Or equals to)
+    == - Equals to
+    != - Not equals to
+
+3=> Examples
+    - "if grade > 80"
+    - "if grade == 80"
+    - "if cash != 0"
+
+
+*/
+
